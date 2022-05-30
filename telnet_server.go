eb16ca2c8259a6140e41847a709619a8f429d2de
Albert Koczy <albert.koczy@asseco.pl>
2022-05-30 21:29:35 +0200

    Add basic botnect command recognition

--- telnet_server.go
+++ telnet_server.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"regexp"
 	"strings"
 	"time"
 )
@@ -122,7 +123,28 @@ func handleTelnetConnection(conn net.Conn) {
 			panic(err)
 		}
 
-		write("OK\n")
+		r := regexp.MustCompile("cat /proc/mounts; /bin/busybox ([A-Za-z0-9]+)")
+		submatches := r.FindStringSubmatch(line)
+		if len(submatches) > 0 {
+			attack.Classification = "command_executed"
+			write(fmt.Sprintf(`/dev/root /rom squashfs ro,relatime 0 0
+proc /proc proc rw,nosuid,nodev,noexec,noatime 0 0
+sysfs /sys sysfs rw,nosuid,nodev,noexec,noatime 0 0
+cgroup2 /sys/fs/cgroup cgroup2 rw,nosuid,nodev,noexec,relatime,nsdelegate 0 0
+tmpfs /tmp tmpfs rw,nosuid,nodev,noatime 0 0
+/dev/mtdblock6 /overlay jffs2 rw,noatime 0 0
+overlayfs:/overlay / overlay rw,noatime,lowerdir=/,upperdir=/overlay/upper,workdir=/overlay/work 0 0
+tmpfs /dev tmpfs rw,nosuid,relatime,size=512k,mode=755 0 0
+devpts /dev/pts devpts rw,nosuid,noexec,relatime,mode=600,ptmxmode=000 0 0
+debugfs /sys/kernel/debug debugfs rw,noatime 0 0
+none /sys/fs/bpf bpf rw,nosuid,nodev,noexec,noatime,mode=700 0 0
+%v: applet not found
+`, submatches[1]))
+			continue
+		} else {
+			write("OK\r\n")
+		}
+
 	}
 
 }
